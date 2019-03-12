e2ba520b1e9fea78ab158bfcec38b4d45fc3d651
Maria <maria_suxareva@mail.ru>
2019-03-12 16:00:04 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.6/firebase-messaging.js');
 
 firebase.initializeApp({
   messagingSenderId: '895966208818'
